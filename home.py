f0b25733fce81a5a7c30cde065f5dd3c7441d8de
JonasCaetanoDeSouza <jonascaaetanodesouzajfgh@gmail.com>
2025-11-12 16:38:37 -0300

    feat: adiciona aba de configurações com opções e layout aprimorado

--- home.py
+++ home.py
@@ -1,18 +1,27 @@
-from PyQt5.QtWidgets import QWidget, QHBoxLayout
+import os
+from PyQt5.QtWidgets import QMainWindow, QWidget, QHBoxLayout, QMenuBar, QAction
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtCore import QUrl
-import os
 
-class Home(QWidget):
+
+class Home(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("JonasCaetano • WebView Dashboard")
         self.setGeometry(100, 100, 1200, 700)
 
-        # Layout horizontal principal
-        layout = QHBoxLayout(self)
+        # === Cria o menu ===
+        menubar = self.menuBar()
+        opcoes_menu = menubar.addMenu("Opções")
 
-        # Remove margens e espaçamento entre os widgets
+        # Ação de configurações
+        config_action = QAction("Configurações", self)
+        config_action.triggered.connect(self.abrir_configuracoes)
+        opcoes_menu.addAction(config_action)
+
+        # === Layout central ===
+        central_widget = QWidget()
+        layout = QHBoxLayout(central_widget)
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
 
@@ -25,8 +34,19 @@ class Home(QWidget):
         self.main_view = QWebEngineView()
         self.main_view.setUrl(QUrl("https://web.whatsapp.com/"))
 
-        # Adiciona ao layout com proporção 25/75
+        # Adiciona as views ao layout
         layout.addWidget(self.sidebar, 25)
         layout.addWidget(self.main_view, 75)
 
-        self.setLayout(layout)
+        # Define o layout central da janela
+        self.setCentralWidget(central_widget)
+
+    def abrir_configuracoes(self):
+        """Abre o settings.html no WebView maior"""
+        settings_path = os.path.abspath("settings.html")
+        if os.path.exists(settings_path):
+            self.main_view.setUrl(QUrl.fromLocalFile(settings_path))
+        else:
+            self.main_view.setHtml(
+                "<h3 style='color:white;text-align:center;margin-top:40px;'>Arquivo settings.html não encontrado</h3>"
+            )

aa8f9a9508d403d1a0bbfe201463eb0a3307a46e
JonasCaetanoDeSouza <jonascaaetanodesouzajfgh@gmail.com>
2025-11-29 17:44:16 -0300

    refactor: remover arquivos antigos

--- scripts/check_block.js
+++
@@ -1,9 +0,0 @@
-var ElementReference = document.getElementById("pane-side")
-var phone = "@PHONE"
-
-if (ElementReference === null){
-
-    let xhr = new XMLHttpRequest()
-    xhr.open("GET", `/maturador/api/account-blocked/{"phone":"${phone}"}`, true)
-    xhr.send()
-}
